61b5c81154bf287fb186967a0c5f5bae24932995
gayoungyeom <gayoungyeom@gmail.com>
2020-11-02 15:53:07 +0900

    [Create] sendMessage module

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
+
 const { ApolloServer, gql} = require('apollo-server-express');
-const express = require("express");
+const express = require('express');
+const { sendMessage } = require('./sendMessage');
 
 const app = express();
 const port = 3000;
 
 const schema = require('./graphql/schemas');
-const models = require("./models/index.js");
+const models = require('./models/index.js');
 
 const server = new ApolloServer({
   schema,
@@ -19,12 +22,13 @@ app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
-app.listen(port, () => {
-    console.log(`app is running at http://localhost:${port}`);
-});
-
 models.sequelize.sync().then( () => {
   console.log("Connection has been established successfully.");
+  //DB연결을 확인 후 서버 실행하는게 좋음
+  app.listen(port, () => {
+      console.log(`app is running at http://localhost:${port}`);
+  });
+  sendMessage('01057584889', '45678');
 }).catch(err => {
   console.log("Unable to connect to the database:");
   console.log(err);
